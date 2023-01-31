398268c69bee815ec4a6855630f040089f9217be
rousopoulosk <konstantinos.rousopoulos@workable.com>
2023-01-31 17:32:03 +0200

    address linting issues

--- pkg/provider/vault/symlink.go
+++ pkg/provider/vault/symlink.go
@@ -1,3 +1,16 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package vault
 
 import (
@@ -7,18 +20,18 @@ import (
 )
 
 const (
-  // Symlink must start with the prefix vault://
-	vaultSymlink        = `vault://`
-  // Path can be anything and matches the last # as a separator of key
-	vaultSymlinkPath    = `(?P<Path>.*)#`
-  // Key can be any alphanumeric character and stops with the first @
-	vaultSymlinkSecret  = `(?P<Secret>\w+)`
-  // Version is optional and will match any number after @
+	// Symlink must start with the prefix vault:// to be valid.
+	vaultSymlink = `vault://`
+	// Path can be anything and matches the last # as a separator of key.
+	vaultSymlinkPath = `(?P<Path>.*)#`
+	// Key can be any alphanumeric character and stops with the first @.
+	vaultSymlinkSecret = `(?P<Secret>\w+)`
+	// Version is optional and will match any number after @.
 	vaultSymlinkVersion = `(@(?P<Version>\d+)?)?`
 	vaultSymlinkPattern = vaultSymlink + vaultSymlinkPath + vaultSymlinkSecret + vaultSymlinkVersion
 )
 
-// isSymlink tests if secret can be converted to string and if it matches the symlink pattern
+// isSymlink tests if secret can be converted to string and if it matches the symlink pattern.
 func isSymlink(secret any) bool {
 	if s, ok := secret.(string); ok {
 		return strings.HasPrefix(s, vaultSymlink)
@@ -27,7 +40,7 @@ func isSymlink(secret any) bool {
 	return false
 }
 
-// extractSymlinkParts extract capture group items of regex to a map
+// extractSymlinkParts extract capture group items of regex to a map.
 func extractSymlinkParts(secret any) (paramsMap map[string]string) {
 	r := regexp.MustCompile(vaultSymlinkPattern)
 	match := r.FindStringSubmatch(secret.(string))
