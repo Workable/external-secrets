71d1e86af858eb30b9a37adaca8a8c4f2d363d75
Nikolaos Stamatopoulos <nikos.stamatopoulos@workable.com>
2024-10-16 17:56:39 +0300

    feat: Add symlink functionality to vault provider

--- pkg/provider/vault/client_get.go
+++ pkg/provider/vault/client_get.go
@@ -70,6 +70,12 @@ func (c *client) GetSecret(ctx context.Context, ref esv1.ExternalSecretDataRemot
 		}
 	}
 
+	// Replace symlinks
+	data, err = c.resolveSymlink(ctx, data)
+	if err != nil {
+		return nil, err
+	}
+
 	return getSecretValue(data, ref.Property)
 }
 
